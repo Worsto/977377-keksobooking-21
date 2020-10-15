ace7f893b8b7b3e5500e7177ed0c973b25c209b0
Worsto <worsto.inbox@gmail.com>
2020-10-15 16:29:19 +0300

    module3

---
+++ js/main.js
@@ -0,0 +1,125 @@
+'use strict';
+
+const OFFERS_QUANTITY = 8;
+const APARTMENT_TYPES = [`palace`, `flat`, `house`, `bungalow`];
+const CHECK_TIMES = [`12:00`, `13:00`, `14:00`];
+const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
+const PICTURES = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
+
+function addZeros(n, needLength) {
+  needLength = needLength || 2;
+  n = String(n);
+  while (n.length < needLength) {
+    n = `0` + n;
+  }
+  return n;
+}
+
+function shuffleArray(array) {
+  let j; let temp;
+  for (let i = array.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+  }
+  return array;
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max + 1 - min) + min);
+}
+
+function getRandomFromArray(array) {
+  return array[getRandomNumber(0, array.length - 1)];
+}
+
+function getRandomsFromArray(array) {
+  let arr = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (Math.round(Math.random()) > 0) {
+      arr.push(array[i]);
+    }
+  }
+
+  return arr;
+}
+
+function createArrayOfAvatarNumbers(amount) {
+  const array = [];
+  for (let i = 1; i <= amount; i++) {
+    array.push(addZeros(i));
+  }
+  shuffleArray(array);
+  return array;
+}
+
+
+function createOfferMok(n) {
+  const locX = getRandomNumber(31, 1169);
+  const locY = getRandomNumber(130, 630);
+
+  return {
+    author: {
+      avatar: `img/avatars/user${avatarNumbers[n]}.png`
+    },
+    offer: {
+      title: `Заголовок предложения`,
+      address: `${locX}, ${locY}`,
+      price: getRandomNumber(1, 120) * 1000,
+      type: getRandomFromArray(APARTMENT_TYPES),
+      rooms: getRandomNumber(1, 4),
+      guests: getRandomNumber(1, 3),
+      checkin: getRandomFromArray(CHECK_TIMES),
+      checkout: getRandomFromArray(CHECK_TIMES),
+      features: getRandomsFromArray(FEATURES),
+      description: `строка с описанием`,
+      photos: getRandomsFromArray(PICTURES)
+    },
+    location: {
+      x: locX,
+      y: locY
+    }
+  };
+}
+
+function createOffers(amount) {
+  const offers = [];
+  for (let i = 0; i < amount; i++) {
+    offers.push(createOfferMok(i));
+  }
+
+  return offers;
+}
+
+function renderPin(offer) {
+  const pinElement = pin.cloneNode(true);
+
+  pinElement.querySelector(`.map__pin`).setAttribute(`style`, `left: ${offer.location.x - 25}px; top: ${offer.location.y - 35}px;`);
+  pinElement.querySelector(`.map__pin`).querySelector(`img`).setAttribute(`src`, offer.author.avatar);
+  pinElement.querySelector(`.map__pin`).querySelector(`img`).setAttribute(`alt`, offer.offer.title);
+
+
+  return pinElement;
+}
+
+function createPinsList(array) {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < array.length; i++) {
+    fragment.appendChild(renderPin(array[i]));
+  }
+
+  return fragment;
+}
+
+const pin = document.querySelector(`#pin`)
+  .content;
+const map = document.querySelector(`.map`);
+
+function showMap(element) {
+  element.classList.remove(`map--faded`);
+}
+const avatarNumbers = createArrayOfAvatarNumbers(OFFERS_QUANTITY);
+const offers = createOffers(OFFERS_QUANTITY);
+showMap(map);
+map.appendChild(createPinsList(offers));
