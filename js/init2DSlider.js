474b38c4de81fe73dff0f43992df6c6c6371992a
Worsto <worsto.inbox@gmail.com>
2020-11-13 22:47:37 +0300

    module5-task2

---
+++ js/init2DSlider.js
@@ -0,0 +1,52 @@
+'use strict';
+
+(function () {
+  function init2DSlider(evt, element, field, elementHeight, fieldTop, FieldBottom, mouseDownFunction, mouseMoveFunction) {
+    mouseDownFunction();
+
+    let shift = {
+      x: evt.clientX - element.getBoundingClientRect().left,
+      y: evt.clientY - element.getBoundingClientRect().top
+    };
+
+    const onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      mouseMoveFunction();
+
+      let newLeft = moveEvt.clientX - shift.x - field.getBoundingClientRect().left;
+      let newTop = moveEvt.clientY - shift.y - field.getBoundingClientRect().top;
+
+      if (newLeft < 0) {
+        newLeft = 0;
+      }
+
+      let rightEdge = field.offsetWidth - element.offsetWidth;
+
+      if (newLeft > rightEdge) {
+        newLeft = rightEdge;
+      }
+
+      if (newTop < fieldTop - elementHeight) {
+        newTop = fieldTop - elementHeight;
+      }
+      if (newTop > FieldBottom - elementHeight) {
+        newTop = FieldBottom - elementHeight;
+      }
+
+      element.style.left = newLeft + `px`;
+      element.style.top = newTop + `px`;
+
+    };
+
+    const onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener(`mousemove`, onMouseMove);
+      document.removeEventListener(`mouseup`, onMouseUp);
+    };
+    document.addEventListener(`mousemove`, onMouseMove);
+    document.addEventListener(`mouseup`, onMouseUp);
+  }
+
+  window.init2DSlider = init2DSlider;
+})();
--- js/pin.js
+++ js/pin.js
@@ -9,59 +9,10 @@
   const mainPinArrow = 22;
   const mainPinHeight = mainPinDiameter + mainPinArrow;
 
-  // перетаскивание
-  function dragMainPin(evt) {
-    window.closeCard();
-
-    let shift = {
-      x: evt.clientX - mainPin.getBoundingClientRect().left,
-      y: evt.clientY - mainPin.getBoundingClientRect().top
-    };
-
-    const onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-      window.form.completeAddressInput();
-
-      let newLeft = moveEvt.clientX - shift.x - mapRectangle.getBoundingClientRect().left;
-      let newTop = moveEvt.clientY - shift.y - mapRectangle.getBoundingClientRect().top;
-
-      if (newLeft < 0) {
-        newLeft = 0;
-      }
-
-      let rightEdge = mapRectangle.offsetWidth - mainPin.offsetWidth;
-
-      if (newLeft > rightEdge) {
-        newLeft = rightEdge;
-      }
-
-      if (newTop < window.mapBorders.top - mainPinHeight) {
-        newTop = window.mapBorders.top - mainPinHeight;
-      }
-      if (newTop > window.mapBorders.bottom - mainPinHeight) {
-        newTop = window.mapBorders.bottom - mainPinHeight;
-      }
-
-      mainPin.style.left = newLeft + `px`;
-      mainPin.style.top = newTop + `px`;
-
-    };
-
-    const onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-
-      document.removeEventListener(`mousemove`, onMouseMove);
-      document.removeEventListener(`mouseup`, onMouseUp);
-    };
-    document.addEventListener(`mousemove`, onMouseMove);
-    document.addEventListener(`mouseup`, onMouseUp);
-  }
-  // перетаскивание
-
   function onPinLeftClick(e) {
     if (typeof e === `object` && e.button === 0) {
       window.page.activatePage();
-      dragMainPin(e);
+      window.init2DSlider(e, mainPin, mapRectangle, mainPinHeight, window.mapBorders.top, window.mapBorders.bottom, window.closeCard, window.form.completeAddressInput);
     }
   }
 
